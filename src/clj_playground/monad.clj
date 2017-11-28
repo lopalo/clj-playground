3051bebc2a30f9bec4793880720779e03d734c37
Vlad Lopalo <vlad.lopalo@gmail.com>
2017-11-28 14:46:35 -0500

    Continuation monad

--- src/clj_playground/monad.clj
+++ src/clj_playground/monad.clj
@@ -7,16 +7,19 @@
   (bind [this reaction])
   (tail-call [this]))
 
-(def >>= bind)
+(defn >>= [monad reaction]
+  (if (satisfies? Monad monad)
+    (bind monad reaction)
+    (reaction monad)))
 
 (defmacro >> [monad monad']
-  `(bind ~monad (fn [~'_] ~monad')))
+  `(>>= ~monad (fn [~'_] ~monad')))
 
 (defn >>' [monad monad']
   (>> monad monad'))
 
 (defn =<< [reaction monad]
-  (bind monad reaction))
+  (>>= monad reaction))
 
 (def tc tail-call)
 
@@ -65,6 +68,8 @@
     (recur ((.function ^TailCall value)))
     value))
 
+(def tco tail-call-loop)
+
 (defrecord State [computation]
   Monad
   (bind [this reaction]
@@ -86,19 +91,26 @@
   Monad
   (bind [this reaction]
     (Cont. (fn [cont]
-             (this (fn [value]
-                     ((reaction value) cont))))))
+             (computation
+              (fn [value]
+                (let [computation' (.computation ^Cont (reaction value))]
+                  (computation' cont)))))))
+  (tail-call [this]
+    (Cont. (fn [cont]
+             (TailCall. #(computation cont)))))
   clojure.lang.IFn
   (invoke [this cont]
-    (computation cont)))
+    (-> cont computation tail-call-loop)))
 
 (defn ->cont [value]
   (Cont. (fn [cont] (cont value))))
 
 (defn call-cc [f]
-  (Cont. (fn [cont] ((f (fn [value]
-                          (Cont. (fn [_] (cont value)))))
-                     cont))))
+  (Cont. (fn [cont]
+           (let [computation
+                 (.computation ^Cont (f (fn [value]
+                                          (Cont. (fn [_] (cont value))))))]
+             (computation cont)))))
 
 (t/deftest base-test
   (t/testing "State"
@@ -141,32 +153,13 @@
                    (->state [a b])))
               [res stack] ((f :foo) [1 2 3])]
           (t/is (= [:c :b] res))
-          (t/is (= [1 2 :a 100 138 :foo :b] stack))))
-      (t/testing "Tail Call Optimization"
-        (letfn [(f1 [t]
-                  (if (< t 30000)
-                    (do>
-                     (st-push [t :a])
-                     (st-push [t :b])
-                     (st-push [t :c])
-                     (st-push [t :d])
-                     (tc (f2 t)))
-                    (->state t)))
-                (f2 [t]
-                    (do>
-                     (st-push :qqq)
-                     (f1 (inc t))))]
-          (let [[res stack] ((f1 0) [])
-                len (count stack)]
-            (t/is (= 30000 res))
-            (t/is (= 150000 len))
-            (t/is (= [:qqq [29999 :a] [29999 :b] [29999 :c] [29999 :d] :qqq]
-                     (subvec stack (- len 6)))))))))
+          (t/is (= [1 2 :a 100 138 :foo :b] stack))))))
+
   (t/testing "Cont"
     (t/testing "Base"
       (let [ct-m
             (>>=
-             (Cont. (fn [c] (concat (c [1]) (c [80]))))
+             (Cont. (fn [c] (into (c [1]) (c [80]))))
              (fn [t1]
                (>>=
                 (->cont (conj t1 2))
@@ -174,8 +167,71 @@
                   (>>=
                    (->cont (conj t2 7))
                    (fn [t3]
-                     (->cont (concat t3 t2))))))))]
+                     (->cont (into t3 t2))))))))]
         (t/is (= [1 2 7 1 2 80 2 7 80 2] (ct-m identity)))))
-    (t/testing "Do block") ;TODO
-    (t/testing "Tail Call Optimization") ;TODO
-    (t/testing "Suspended computation"))) ;TODO
+    (t/testing "Do block"
+      (let [f (fn [d]
+                (do>
+                 (|> (Cont. (fn [c] (c [1]))) t1)
+                 (|> (->cont (conj t1 2)) t2)
+                 (|> (->cont (conj t2 d)) t3)
+                 (->cont (into t3 t2))))]
+        (t/is (= [1 2 77 1 2] ((f 77) identity)))))
+    (t/testing "Call CC"
+      (let [f (fn [call]
+                (do>
+                 (<| t1 (call-cc
+                         (fn [c]
+                           (do>
+                            (when call (c 100))
+                            (<| t2 (->cont 11))
+                            (->cont (+ t2 200))))))
+                 (->cont (+ t1 12))))]
+        (t/is (= 112 ((f true) identity)))
+        (t/is (= 223 ((f false) identity)))))
+    (t/testing "Tail Call Optimization"
+      (letfn [(add [n] (fn [c] (+ n)))
+              (f1 [t]
+                  (if (< t 5000000)
+                    (do>
+                     (<| a (->cont (+ t 2)))
+                     (<| b (->cont (+ a 1)))
+                     (tc (f2 b)))
+                    (->cont t)))
+              (f2 [t]
+                  (do>
+                   (<| a (->cont (+ t 8)))
+                   (<| b (->cont (+ a 11)))
+                   (f1 b)))]
+        (let [ct-m (do>
+                    (f2 0)
+                    (Cont. (fn [c] (+ (/ (-> nil c tco) 2)
+                                      (/ (-> nil c tco) 2)
+                                      (/ (-> nil c tco) 2))))
+                    (<| a (f1 0))
+                    (<| b (->cont (+ a 13)))
+                    (<| c (f2 b))
+                    (->cont c))]
+          (t/is (= 7500057 (ct-m identity))))))
+    (t/testing "Suspended computation"
+      (let [yield (Cont. identity)
+            ct-m1
+            (do>
+             (<| a yield)
+             (<| b yield)
+             (->cont [a b :a]))
+            ct-m2
+            (do>
+             (<| a yield)
+             (<| b (tc ct-m1))
+             (<| c yield)
+             (->cont (conj b a c :b)))
+            ct-m3
+            (do>
+             (<| a ct-m2)
+             (<| b yield)
+             (<| c yield)
+             (->cont (conj a b c :c)))
+            input (range 6)
+            output (reduce #((tco %1) %2) (ct-m3 identity) input)]
+        (t/is (= [1 2 :a 0 3 :b 4 5 :c] output))))))
